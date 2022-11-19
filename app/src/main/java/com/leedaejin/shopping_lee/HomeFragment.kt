2663fe7e2cfd5810820f92e16acb88c7a7765cd1
ldj4245 <ldj4241@naver.com>
2022-11-20 01:09:15 +0900

    Fragment 추가 및 화면 연결

---
+++ app/src/main/java/com/leedaejin/shopping_lee/HomeFragment.kt
@@ -0,0 +1,33 @@
+package com.leedaejin.shopping_lee
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class HomeFragment:Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val button = view.findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.add(R.id.container_main,ProductDetailFragment())
+            transaction.commit()
+
+        }
+    }
+}
